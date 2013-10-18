61bbc830da4c370afac13108e5e83414ecf81749
baptistelemoine <baptiste.lemoine@gmail.com>
2013-10-18 13:51:12 +0200

    add service directly on scope, and access data from it

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -2,15 +2,6 @@
 
 app.controllers.controller('SearchController',['$scope','SearchManager', function ($scope, SearchManager){
 
-	$scope.launchSearch = function(){
+	$scope.search = SearchManager;
 
-		$scope.items = [];
-
-		var request = SearchManager.search($scope.term);
-		request.then(function (response){
-			angular.forEach(response.data.items, function(value, key){
-				$scope.items.push(value);
-			});
-		});
-	};
 }]);
--- app/js/services/SearchManager.js
+++ app/js/services/SearchManager.js
@@ -1,10 +1,34 @@
 'use strict'
 
 app.services.factory('SearchManager', ['$http', 'ConfigManager', function ($http, ConfigManager){
+	
 	return {
+		
+		items:[],		
 		url:ConfigManager.searchUrl,
-		search:function(term){
-			return $http({method:'GET', cache:true, url:this.url, params:{q:term}});
+		busy:false,
+		term:'',
+		currentPage:0,
+		perPage:10,
+		
+		nextPage:function(term){
+			
+			var self = this;
+
+			this.busy = true;
+			if(this.term !== term) this.items = [];
+
+			var offset = this.currentPage > 0 ? this.currentPage * this.perPage : 0;
+
+			$http.get(this.url, {params:{q:term, offset:offset}, cache:true})
+			.success(function (data){
+				angular.forEach(data.items, function (value, key){
+					self.items.push(value);
+				});
+				self.busy = false;
+				self.term = term;
+				self.currentPage++;
+			});
 		}
 	};
 }]);
