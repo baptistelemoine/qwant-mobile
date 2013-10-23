46d36f353217ecbd9a36cc727792881795d72a86
baptistelemoine <baptiste.lemoine@gmail.com>
2013-10-23 14:59:44 +0200

    config grunt production deployment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt){
 	
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 		less:{
 			developement:{
 				src : ['app/less/app.less'],
@@ -10,9 +11,37 @@ module.exports = function (grunt){
 		watch:{
 			files : ['app/less/*.less'],
 			tasks : ['less']
+		},
+		concat: {
+			options: {
+				stripBanners: true,
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %> */',
+			},
+			dist: {
+				src: ['app/js/**/*.js'],
+				dest: 'app/dist/built.js',
+			},
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %> */'
+			},
+			dist: {
+				files: {
+					'app/dist/app.min.js': ['app/dist/built.js']
+				}
+			}
 		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('dist', ['concat:dist', 'uglify:dist']);
+
 };
---
+++ app/dist/built.js
@@ -0,0 +1,96 @@
+/*! angualrjs-seed - v - 2013-10-23 */'use strict';
+
+// Declare app level module which depends on filters, and services
+var app = angular.module('qwant', [
+  'ngRoute',
+  'qwant.filters',
+  'qwant.services',
+  'qwant.directives',
+  'qwant.controllers',
+  'ngSanitize',
+  'infinite-scroll',
+  'snap'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/search', {templateUrl: 'partials/searchResult.html', controller: 'SearchController'});
+  $routeProvider.otherwise({redirectTo: '/'});
+}]);
+
+app.services = angular.module('qwant.services', []);
+app.controllers = angular.module('qwant.controllers', []);
+'use strict';
+
+app.controllers.controller('SearchController',[
+	'$scope','SearchManager','$location', function ($scope, SearchManager, $location){
+
+	$scope.term = $location.search().q;
+	SearchManager.setSource('news');
+	$scope.search = SearchManager;
+	SearchManager.nextPage($scope.term);
+
+}]);
+'use strict';
+
+/* Directives */
+
+
+var directives = angular.module('qwant.directives', []);
+
+'use strict';
+
+/* Filters */
+
+var filters = angular.module('qwant.filters', []);
+'use strict';
+
+app.services.value('ConfigManager', {
+		searchUrl:'http://localhost:3000/search'
+	}
+);
+'use strict'
+
+app.services.factory('SearchManager', [
+	'$http', 'ConfigManager', '$rootScope', function ($http, ConfigManager, $rootScope){
+	
+	return {
+		
+		items:[],
+		url:ConfigManager.searchUrl,
+		busy:false,
+		term:'',
+		currentPage:0,
+		perPage:10,
+		source:'all',
+
+		setSource:function(src){ this.source = src; },
+
+		resetSearch:function(){
+			this.items = [];
+			this.currentPage = 0;
+		},
+
+		nextPage:function(term){
+
+			var self = this;
+
+			if (this.busy) return;
+			this.busy = true;
+
+			//new search ? reset all
+			if(this.term !== term) this.resetSearch();
+
+			var offset = this.currentPage > 0 ? this.currentPage * this.perPage : 0;
+			
+			$http.get(this.url, {params:{q:term, offset:offset, source:this.source}, cache:true})
+			.success(function (data){				
+				var dataSource = data.items ? data.items : data[self.source];
+				angular.forEach(dataSource, function (value, key){
+					self.items.push(value);
+				});
+				self.term = term;
+				self.currentPage++;
+				self.busy = false;
+			});
+		}
+	};
+}]);
