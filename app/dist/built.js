97c1d52f98eb3eaeec25171584bf01cf89152db4
baptistelemoine <baptiste.lemoine@gmail.com>
2013-10-24 09:29:25 +0200

    compile app

--- app/dist/built.js
+++ app/dist/built.js
@@ -1,4 +1,4 @@
-/*! qwant-mobile - v - 2013-10-23 */'use strict';
+/*! qwant-mobile - v - 2013-10-24 */'use strict';
 
 // Declare app level module which depends on filters, and services
 var app = angular.module('qwant', [
