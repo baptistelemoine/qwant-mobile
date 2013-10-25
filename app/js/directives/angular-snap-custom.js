fa7ab14c0b7ac0286864436d48729bc6e43262fe
baptistelemoine <baptiste.lemoine@gmail.com>
2013-10-25 16:45:31 +0200

    convert to directive, stay header fixed

--- app/js/directives/angular-snap-custom.js
+++ app/js/directives/angular-snap-custom.js
@@ -6,7 +6,7 @@ angular.module('snap').directive('snapDrawers', ['$window', function ($window){
 		link:function(scope, element, attributes){
 
 			$window.addEventListener('scroll', function (event){
-				element.css('top', this.document.documentElement.scrollTop||this.document.body.scrollTop);
+				element.css('top', (this.document.documentElement.scrollTop||this.document.body.scrollTop) + 45);
 			});
 
 			scope.$on('destroy', function(){
