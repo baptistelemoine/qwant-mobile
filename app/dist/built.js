2fda15aa2ce243af8cbc173ce8fcc91d0f1ed93b
baptistelemoine <baptiste.lemoine@gmail.com>
2013-10-25 14:11:12 +0200

    compile app

--- app/dist/built.js
+++ app/dist/built.js
@@ -38,11 +38,20 @@ app.controllers.controller('SearchController',[
 'use strict';
 
 
-app.controllers.controller('SideBarController', ['$scope', function ($scope){
+app.controllers.controller('SideBarController', ['$scope','$window','snapRemote', function ($scope, $window, snapRemote){
 	$scope.user = {
 		'name':'Baptiste Lemoine',
 		'email':'baptiste.lemoine@gmail.com'
 	}
+
+	/*snapRemote.getSnapper().then(function (snapper){
+		var sideBar = $window.document.querySelector('.snap-drawers');
+		console.log(sideBar)
+	});
+	
+	$window.addEventListener('scroll', function (e){
+		// console.log(this.document.body.scrollTop);
+	});*/
 }]);
 'use strict';
 
