4bd2d42f5eb9a6850e184f90a64077241d5b415b
baptistelemoine <baptiste.lemoine@gmail.com>
2013-10-31 14:11:18 +0100

    item renderer OK

--- app/js/filters/filters.js
+++ app/js/filters/filters.js
@@ -5,3 +5,30 @@ app.filters.filter('detailsURL', ['$location', function ($location){
 		return $location.$$absUrl.concat('&_id=', input);
 	};
 }]);
+
+app.filters.filter('host', ['$location', function ($location){
+	return function (input){
+		if(input.indexOf('http') !== 0) return input.split('/')[0];
+		else return input.split('/')[2];
+
+		return input;
+	}
+}]);
+
+app.filters.filter('sce', ['$sce', function ($sce){
+	return function (input){
+		return $sce.trustAsResourceUrl(input);
+	}
+}]);
+
+app.filters.filter('yturlformat', function (){
+	return function (input){
+		return input.concat('?showinfo=0&controls=0')
+	}
+});
+
+app.filters.filter('pricing', function (){
+	return function (input){
+		return input.replace('&euro;', '€');
+	}
+});
