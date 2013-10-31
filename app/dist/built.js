478227cb4906b7a4af84ea78603cf3e2d7ce9e44
baptistelemoine <baptiste.lemoine@gmail.com>
2013-10-31 16:42:22 +0100

    compile app

--- app/dist/built.js
+++ app/dist/built.js
@@ -1,4 +1,4 @@
-/*! qwant-mobile - v - 2013-10-30 */'use strict';
+/*! qwant-mobile - v - 2013-10-31 */'use strict';
 
 // Declare app level module which depends on filters, and services
 var app = angular.module('qwant', [
@@ -133,6 +133,33 @@ app.filters.filter('detailsURL', ['$location', function ($location){
 		return $location.$$absUrl.concat('&_id=', input);
 	};
 }]);
+
+app.filters.filter('host', ['$location', function ($location){
+	return function (input){
+		if(input.indexOf('http') !== 0) return input.split('/')[0];
+		else return input.split('/')[2];
+
+		return input;
+	}
+}]);
+
+app.filters.filter('sce', ['$sce', function ($sce){
+	return function (input){
+		return $sce.trustAsResourceUrl(input);
+	}
+}]);
+
+app.filters.filter('yturlformat', function (){
+	return function (input){
+		return input.concat('?showinfo=0&controls=0')
+	}
+});
+
+app.filters.filter('pricing', function (){
+	return function (input){
+		return input.replace('&euro;', '€');
+	}
+});
 'use strict';
 
 app.services.value('ConfigManager', {
