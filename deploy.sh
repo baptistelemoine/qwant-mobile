e4193dff72fc5b43566cca77463f76853bf53070
baptistelemoine <baptiste.lemoine@gmail.com>
2013-11-06 17:12:48 +0100

    compile app

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,7 @@ git show master:app/dist/app.min.js > app/js/app.min.js
 git show master:app/dist/angular-modules.min.js > app/js/angular-modules.min.js
 git show master:app/dist/vendor.min.js > app/js/vendor.min.js
 git show master:app/dist/app-min.css > app/css/app-min.css
+git show master:server.js > server.js
 #copy entire partial and font folders folder
 git checkout master -- app/partials app/font app/img
 git add app/partials/ app/font app/img
